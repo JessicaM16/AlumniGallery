ded5107d368ec91ea4f441af607182ab0d092076
JessicaM16 <j.mohammed2323@gmail.com>
2022-04-08 15:32:23 +0000

    signup

--- App/views/signUp.py
+++ App/views/signUp.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory
 from flask_jwt import jwt_required
 
-#from App.models.users import db, User
+
+from App.models.user import db, User
 
 signUp_views = Blueprint('signUp_views', __name__, template_folder='../templates')
 
 @signUp_views.route('/signUp')
 def get_signUp():
     userData=request.get_json()
-    newUser= User(username=userDate['username'], email=userDate['email'])
+    newUser= User(username=userData['username'], email=userData['email'])
     newUser.set_password(userData['password'])
     try:
         db.session.add(newUser)
@@ -19,17 +20,3 @@ def get_signUp():
     return 'User created'
 
 
-'''
-@app.route('/signup', methods=['POST'])
-def signup():
-  userdata = request.get_json() # get json data
-  newuser = User(username=userdata['username'], email=userdata['email']) # create user object
-  newuser.set_password(userdata['password']) # set password
-  try:
-    db.session.add(newuser)
-    db.session.commit() # save user
-  except IntegrityError: # attempted to insert a duplicate user
-    db.session.rollback()
-    return 'username or email already exists' # error message
-  return 'user created' # success
-'''
