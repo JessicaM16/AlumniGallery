2c555a117b88813f983ca883fe4414cb4a986e1b
JessicaM16 <j.mohammed2323@gmail.com>
2022-04-14 03:50:55 +0000

    import and method

--- App/views/logIn.py
+++ App/views/logIn.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template, jsonify, request, send_from_directory
+from flask import Blueprint, redirect, render_template, jsonify, request, send_from_directory
 from flask_jwt import JWT, jwt_required, current_identity
 from sqlalchemy.exc import IntegrityError
 
 from App.models.user import db, User
 
+logIn_views = Blueprint('logIn_views', __name__, template_folder='../templates')
 
+@logIn_views.route('/logIn', methods=['POST'])
 def authenticate(uname, password):
   #search for the specified user
   user = User.query.filter_by(username=uname).first()
